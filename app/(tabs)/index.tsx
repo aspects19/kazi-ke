e8ee0b8a26a3388f0fc9cfff13eb45cf28772f76
Amenya <133534815+aspects19@users.noreply.github.com>
2025-07-31 18:02:25 +0300

    Add tailwind styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabOneScreen() {
   return (
     <View  className=' flex-1 content-center items-center text-black'>
-      <Text>Index screen</Text>
+      <SafeAreaView>
+        <Text>Index screen</Text>
+      </SafeAreaView>
+      
     </View>
   );
 }
