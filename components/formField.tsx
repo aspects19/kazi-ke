e0fb148dafd7f78153e532feaba5366d16217059
Amenya <133534815+aspects19@users.noreply.github.com>
2025-08-02 10:15:55 +0300

    Add proper types

--- components/formField.tsx
+++ components/formField.tsx
@@ -1,28 +1,37 @@
 import { View, Text, TextInput } from 'react-native'
 import React, { useState } from 'react'
 
-const FormField = ({
-    
-    title, 
-    value,
-    placeholder,
-    handleChangeText,
-    additionalStyles,
-    ...props
+type FormFieldProps = {
+  title: string
+  value?: string
+  placeholder?: string
+  handleChangeText: (text: string) => void
+  additionalStyles?: string
+  [key: string]: any
+}
+
+const FormField: React.FC<FormFieldProps> = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  additionalStyles,
+  ...props
 }) => {
-    const [showPassword, setshowPassword] = useState(false)
+  const [showPassword, setshowPassword] = useState(false)
   return (
     <View className={` space-y-2 ${additionalStyles}`}>
-        <View className='w-full h-16 px-3 border-red-400 bg-slate-900 border-solid rounded-2xl'>
-            <TextInput
-            className='flex-1 text-white bg-slate-900 font-semibold text-base'
-            placeholder={placeholder}
-            placeholderTextColor='#7b7b8b'
-            onChangeText={handleChangeText}
-            secureTextEntry = {title === "password" && !showPassword}
-            />
-
-        </View>
+      <View className='w-full h-16 px-3 border-red-400 bg-slate-900 border-solid rounded-2xl'>
+        <TextInput
+          className='flex-1 text-white bg-slate-900 font-semibold text-base'
+          placeholder={placeholder}
+          placeholderTextColor='#7b7b8b'
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "password" && !showPassword}
+          value={value}
+          {...props}
+        />
+      </View>
     </View>
   )
 }
